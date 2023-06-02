1e7e6586e6dfe8ba0a8252a4945af171c5882ac4
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-06-02 19:04:22 +0300

    fixed error

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </PersistGate>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
